077f448af2dbc31bf4935874ea1d889005071911
MarkoSelchanec <59167796+MarkoSelchanec@users.noreply.github.com>
2020-07-29 17:56:41 +0200

    added form, some styling and events

--- script.js
+++ script.js
@@ -60,6 +60,9 @@ meAvatarLink.addEventListener("mouseleave", () => {
 // Content-btns
 
 const showPage = (iOne, iTwo, iThree) => {
+    contentBtns[iOne].classList.add("selected");
+    contentBtns[iTwo].classList.remove("selected");
+    contentBtns[iThree].classList.remove("selected");
     contentPages[iOne].classList.remove("hide");
     contentPages[iTwo].classList.add("hide");
     contentPages[iThree].classList.add("hide"); 
@@ -68,9 +71,58 @@ const showPage = (iOne, iTwo, iThree) => {
 contentBtns[0].addEventListener("click", () => showPage(0, 1, 2));
 projectsBtns.forEach(e => e.addEventListener("click", () => showPage(0, 1, 2)));
 
-
 contentBtns[1].addEventListener("click", () => showPage(1, 0, 2));
 skillsBtns.forEach(e => e.addEventListener("click", () => showPage(1, 0, 2)));
 
 contentBtns[2].addEventListener("click", () => showPage(2, 1, 0));
 contactBtns.forEach(e => e.addEventListener("click", () => showPage(2, 1, 0)));
+
+
+// FORM email
+
+window.addEventListener("DOMContentLoaded", function() {
+
+    // get the form elements defined in your form HTML above
+    
+    var form = document.getElementById("my-form");
+    var button = document.getElementById("my-form-button");
+    var status = document.getElementById("my-form-status");
+    var subject = document.getElementById("form-subject");
+    var email = document.getElementById("form-email");
+    var message = document.getElementById("form-message");
+    
+    function success() {
+      form.reset();
+      button.style = "display: none ";
+      status.innerHTML = "Thank you, I will contact you shortly!";
+    }
+
+    function error() {
+        status.innerHTML = "Oops, something went wrong.";
+    }
+
+    form.addEventListener("submit", function(ev) {
+        ev.preventDefault();
+        if (!subject.value || !email.value || message.value.length < 5) {
+            status.innerHTML = "Please fill out all fields.";
+        } else {
+            var data = new FormData(form);
+            ajax(form.method, form.action, data, success, error);
+        }
+    });
+});
+
+function ajax(method, url, data, success, error) {
+    var xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState !== XMLHttpRequest.DONE) return;
+        if (xhr.status === 200) {
+        success(xhr.response, xhr.responseType);
+        } else {
+        error(xhr.status, xhr.response, xhr.responseType);
+        } 
+    };
+xhr.send(data);
+}
